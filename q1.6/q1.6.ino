31d084b454071da7d435fd0c8da7e5f6c67d1e1b
calvinterpstra <calvinterpstra@gmail.com>
2019-02-24 17:30:52 +0100

    work on 1.6

---
+++ q1.6/q1.6.ino
@@ -0,0 +1,71 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+char getNextState(){
+    int incomingByte = 0;
+    if (Serial.available() > 0) {
+        incomingByte = Serial.read();
+        if (incomingByte == 87 || incomingByte == 82 || incomingByte == 66 || incomingByte == 83 || incomingByte == 70){
+            return char(incomingByte);
+        }
+    }
+    else {
+        return ;
+    }
+}
+
+void Rest(){
+    Serial.println("Rest");
+}
+
+void Step(){
+    Serial.println("Step");
+}
+
+void Follow(){
+    Serial.println("Follow");
+}
+
+void Blink(){
+    Serial.println("Blink");
+}
+
+void Switch(){
+    Serial.println("Switch");
+}
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(3);
+}
+
+char state = 'R';
+void loop() {
+    switch (state) {
+        case 'R':
+            Rest();
+            break;
+        case 'S':
+            Step();
+            break;
+        case 'F':
+            Follow();
+            break;
+        case 'B':
+            Blink();
+            break;
+        case 'W':
+            Switch();
+            break;
+        default:
+            Serial.print("default: ");
+            Serial.println(state);
+            break;
+    }
+    state = getNextState();
+    delay(100);
+}
