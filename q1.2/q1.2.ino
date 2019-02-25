f7d359098b3dc803fe744dd6c67b25a8b5002c8d
Quinten <thequintendoornbos@gmail.com>
2019-02-25 12:02:09 +0100

    sd

--- q1.2/q1.2.ino
+++ q1.2/q1.2.ino
@@ -1,10 +1,6 @@
 #include <Servo.h>
-#include <Event.h>
-#include <Timer.h>
 
 Servo myservo;
-
-Timer t;
 
 int posX = 60;
 int posY = 90;
@@ -14,8 +10,8 @@ void setup() {
 }
 
 void loop() {
-  t.every(1000,myservo.write(posX));              // tell servo to go to position X
- // delay(1000);                       // waits 1000ms for the servo to reach the position
-  t.every(1000,myservo.write(posY));              // tell servo to go to position Y
-//  delay(1000);                       // waits 1000ms for the servo to reach the position
+  myservo.write(posX);        
+  delay(1000);                    
+  1000,myservo.write(posY);           
+  delay(1000);                
   }
--- q1.4/q1.4.ino
+++ q1.4/q1.4.ino
@@ -15,6 +15,14 @@ int Analog2Angle(int analogInput){
 void loop() {
   int v = analogRead(A0);
   
-  Serial.println(Analog2Angle(v));
+//  Serial.println(Analog2Angle(v));
+  int voltage = map(v,0,1023,5000,0);
+  int angle = map(v,0,1023,270,0);
+  Serial.print(voltage);
+  Serial.print(", ");
+  Serial.print(angle);
+  Serial.print(", ");
+  Serial.println(v);
+  delay(500);
   myservo.write(Analog2Angle(v));
   }
