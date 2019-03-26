13422ab98f06b02b5d73e88a1c2474a564a5ad2f
Quinten <thequintendoornbos@gmail.com>
2019-03-26 13:33:43 +0100

    hpoi

--- digital filter.py
+++ digital filter.py
@@ -3,14 +3,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# R = 2.2 * 10**3
-# a = 3.95 * 10**-3
-# b = 6.28 * 10**-2
-# print("1: ", 5 / np.sqrt(a*R**2 + 1))
-# print("2: ", np.arctan(b*R))
+R = 2200
+C = 1e-6
+f_c = 1/(2*np.pi*R*C) 	#analog cut-off frequency (Hz)
+omega_c = 2*np.pi*f_c	#(rad/s)
+T_c = 2*pi/omega_c 		#period (s)
+omega_d = omega_c		#desired cut-off frequency (rad/s)
 
-C = 1 * 10**-6
-L = 13 * 10**-6
-R = 220
 
-print((np.sqrt(1/(L*C))/(2*np.pi)))
+omega_a = (2/T_c)*np.tan((omega_d)/2) #analog required frequency (rad/s)
+f_a = (2*np.pi)/omega_a
+
