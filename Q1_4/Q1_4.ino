c40f456b5b0cd564fedd89a2e8fb981d57ae64e0
calvinterpstra <calvinterpstra@gmail.com>
2019-03-26 12:20:47 +0100

    worked on pracucum 5

--- Q1_4/Q1_4.ino
+++ Q1_4/Q1_4.ino
@@ -23,6 +23,24 @@ float first_filt(float x0){
   return y0;
 }
 
+float second_filt(float x0){
+  static float y1=0;  // y[n-1]
+  static float y2=0;  // y[n-2]
+  static float x1=0;  // x[n-1]
+  static float x2=0;  // x[n-2]
+  
+  float y0 = 0; // TODO: Fill in this line
+  
+  // Shift values
+  y2 = y1;  // Shift y[n-1] to y[n-2]
+  y1 = y0;  // Shift y[n] to y[n-1]
+  x2 = x1;  // Shift x[n-1] to x[n-2]
+  x1 = x0;  // Shift x[n] to x[n-1]
+  
+  return y0;
+}
+
+
 void setup() {
   Timer1.initialize(32); //Initialize timer with a period of 32 us
   Timer1.pwm(9,0); // Use pin 9 for PWM
