0d6d8e164f1fb33890d93f2e2b9f0c03e6db998a
Quinten <thequintendoornbos@gmail.com>
2019-03-26 13:34:57 +0100

    hoi

--- digital filter.py
+++ digital filter.py
@@ -7,7 +7,7 @@ R = 2200
 C = 1e-6
 f_c = 1/(2*np.pi*R*C) 	#analog cut-off frequency (Hz)
 omega_c = 2*np.pi*f_c	#(rad/s)
-T_c = 2*pi/omega_c 		#period (s)
+T_c = 2*np.pi/omega_c 		#period (s)
 omega_d = omega_c		#desired cut-off frequency (rad/s)
 
 
