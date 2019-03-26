25806b537144db95bbca4cf7162d71df21365dd9
calvinterpstra <calvinterpstra@gmail.com>
2019-03-26 16:50:25 +0100

    added 2nd order stuff

--- Practicum 5/Q1_4/Q1_4.ino
+++ Practicum 5/Q1_4/Q1_4.ino
@@ -1,4 +1,5 @@
 #include <TimerOne.h>
+#include <math.h>
 
 bool isTime(){ //Checks if 300 us elapsed since the last call
   static unsigned long t0=0;
@@ -15,8 +16,12 @@ float first_filt(float x0){
   static float y1=0;  // y[n-1]
   static float x1=0;  // x[n-1]
   
-  float y0 = 0.34817*x0 + 0.34917*x1 + 0.30166*y1; // TODO: Fill in this line
-  
+  float k_x0 = 0.31353; // 0.6507
+  float k_x1 = 0.31353; // 0.6507
+  float k_y1 = 0.37293; // -0.3013
+
+  float y0 = k_x0*x0 + k_x1*x1 + k_y1*y1; // TODO: Fill in this line
+
   y1 = y0;  // Shift y[n] to y[n-1]
   x1 = x0;  // Shift x[n] to x[n-1]
   
@@ -29,7 +34,13 @@ float second_filt(float x0){
   static float x1=0;  // x[n-1]
   static float x2=0;  // x[n-2]
   
-  float y0 = 0; // TODO: Fill in this line
+  float k_x0 = ; // 
+  float k_x1 = ; // 
+  float k_y1 = ; // 
+  float k_x2 = ; // 
+  float k_y2 = ; // 
+
+  float y0 = k_x0*x0 + k_x1*x1 + k_y1*y1 + k_x2*x2 + k_y2*y2; // TODO: Fill in this line
   
   // Shift values
   y2 = y1;  // Shift y[n-1] to y[n-2]
